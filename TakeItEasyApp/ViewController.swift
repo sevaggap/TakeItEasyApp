f94176d3aa6b11ab0a16aa09399a7c4fac226690
sevaggap <sevag.gaprielian@gmail.com>
2022-06-07 15:58:56 -0400

    Initial Commit

---
+++ TakeItEasyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TakeItEasyApp
+//
+//  Created by Sevag Gaprielian on 2022-06-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
