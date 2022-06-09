ee5737785e7c8ffd0428fcffbfa2f7f440540272
Bristy Mitra <mitra.bristy@outlook.com>
2022-06-09 11:01:12 -0400

    setup sign in, sign up

--- TakeItEasyApp/SignupViewController.swift
+++ TakeItEasyApp/SignupViewController.swift
@@ -8,7 +8,27 @@
 import UIKit
 
 class SignupViewController: UIViewController {
-
+      var errorMsgSignUp = ""
+    
+    @IBOutlet weak var userName: UITextField!
+    
+    @IBOutlet weak var userEmail: UITextField!
+    
+    @IBOutlet weak var userPassword: UITextField!
+    
+    @IBOutlet weak var re_enterPassword: UITextField!
+    
+    
+    @IBOutlet weak var userMobile: UITextField!
+    
+    
+    @IBAction func getOTP(_ sender: Any) {
+    }
+    
+    @IBAction func signIn(_ sender: Any) {
+        //signup validation
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +36,6 @@ class SignupViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
 }
