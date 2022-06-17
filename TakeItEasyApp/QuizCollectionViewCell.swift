758236fdacfeb751bf4387630f99892c5630b75b
Xavier <32859092+xavier7t@users.noreply.github.com>
2022-06-17 16:14:24 -0400

    -Quiz tab updates -Main storyboard config updates

---
+++ TakeItEasyApp/QuizCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  QuizCollectionViewCell.swift
+//  TakeItEasyApp
+//
+//  Created by Xavier on 6/16/22.
+//
+
+import UIKit
+
+class QuizCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var labelQuizID: UILabel!
+    @IBOutlet weak var imageViewThumbnail: UIImageView!
+    @IBOutlet weak var labelQuizDescription: UILabel!
+    @IBOutlet weak var imageViewCompleted: UIImageView!
+}
