af335727866f9808dea12be6c5ace776f7efade0
James-Suresh <dev.jamessuresh@gmail.com>
2022-06-20 01:54:07 -0400

    Revert "Zahid3"

--- TakeItEasyApp/ZahidBook/BooksViewController.swift
+++ TakeItEasyApp/ZahidBook/BooksViewController.swift
@@ -1,240 +1,79 @@
 import UIKit
 import PDFKit
-import WebKit
 
 
+//variable data for book covers and pdf files
 
-var genBookIMG = ["gb1", "gb2", "gb3", "gb4", "gb5"]
-var techBookIMG = ["tb1", "tb2", "tb3", "tb4", "tb5"]
-var recBookIMG = ["rb1", "rb2", "rb3", "rb4", "rb5"]
+var bookIMG = ["hp1", "hp2", "hp3"]
+var bookData = ["hpbook1", "hpbook2", "hpbook3"]
 var pdfView : PDFView? = nil
-
-
-//var bookLink : String?
-var genBookLink : [String]?
-var techBookLink : [String]?
-var recBookLink : [String]?
-
-
-var localBooks = ["general", "technical", "recipe"]
-
-
-
-var searching = false
 
 
 class BooksViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
     
-    @IBOutlet weak var searchBar: UISearchBar!
+    //collection view for each selection
     
     @IBOutlet weak var CV1: UICollectionView!
     @IBOutlet weak var CV2: UICollectionView!
     @IBOutlet weak var CV3: UICollectionView!
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return recBookIMG.count
+        return bookIMG.count
+        //number of books in collection
        
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        //3 cells: Each cell represents a different topic of book
         
         if collectionView == self.CV1{
         
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell1", for: indexPath) as! GeneralCollectionViewCell
-                cell.bookImage.image = UIImage(named: genBookIMG[indexPath.row])
-                cell.contentMode = .scaleAspectFit
-                cell.layer.cornerRadius = 15.0
-                cell.layer.borderWidth = 0.0
-                cell.layer.shadowColor = UIColor.black.cgColor
-                cell.layer.shadowOffset = CGSize(width: 0, height: -1)
-                cell.layer.shadowRadius = 5.0
-                cell.layer.shadowOpacity = 1
-                cell.layer.masksToBounds = true
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell1", for: indexPath) as! GeneralCollectionViewCell
+        cell.bookImage.image = UIImage(named: bookIMG[indexPath.row])
             return cell
-            
-            
         }; if collectionView == self.CV2{
             
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell2", for: indexPath) as! TechnicalCollectionViewCell
-            cell.bookImage.image = UIImage(named: techBookIMG[indexPath.row])
-            cell.contentMode = .scaleAspectFit
-            cell.layer.cornerRadius = 15.0
-            cell.layer.borderWidth = 0.0
-            cell.layer.shadowColor = UIColor.black.cgColor
-            cell.layer.shadowOffset = CGSize(width: 0, height: -1)
-            cell.layer.shadowRadius = 5.0
-            cell.layer.shadowOpacity = 1
-            cell.layer.masksToBounds = true
+            cell.bookImage.image = UIImage(named: bookIMG[indexPath.row])
             return cell
-            
         }else{
-            
             //collectionView == self.CV3
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell3", for: indexPath) as! RecipesCollectionViewCell
-            cell.bookImage.image = UIImage(named: recBookIMG[indexPath.row])
-            cell.contentMode = .scaleAspectFit
-            cell.layer.cornerRadius = 15.0
-            cell.layer.borderWidth = 0.0
-            cell.layer.shadowColor = UIColor.black.cgColor
-            cell.layer.shadowOffset = CGSize(width: 0, height: -1)
-            cell.layer.shadowRadius = 5.0
-            cell.layer.shadowOpacity = 1
-            cell.layer.masksToBounds = true
+                       let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell3", for: indexPath) as! RecipesCollectionViewCell
+                    cell.bookImage.image = UIImage(named: bookIMG[indexPath.row])
                 return cell
                 
     }
         
-}
+        
+        
+    }
     
     
 
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
-        if collectionView == self.CV1{
+        //user clicks on cell to open pdf file in seperate view
+        
         switch indexPath.row{
         case 0 :
             
             let storyObject = UIStoryboard(name: "Zahid", bundle: nil)
-            let webKitVC = storyObject.instantiateViewController(withIdentifier: "webScreen") as! WebUIKit
-  
-            webKitVC.bookLink = genBookLink![indexPath.row]
-            
-            self.present(webKitVC, animated: true, completion: nil)
-            
-            
-        case 1:
-            let storyObject = UIStoryboard(name: "Zahid", bundle: nil)
-            let webKitVC = storyObject.instantiateViewController(withIdentifier: "webScreen") as! WebUIKit
-         
-            webKitVC.bookLink = genBookLink![indexPath.row]
-            
-            self.present(webKitVC, animated: true, completion: nil)
-            
-
-        case 2:
-            let storyObject = UIStoryboard(name: "Zahid", bundle: nil)
-            let webKitVC = storyObject.instantiateViewController(withIdentifier: "webScreen") as! WebUIKit
-         
-            webKitVC.bookLink = genBookLink![indexPath.row]
-            
-            self.present(webKitVC, animated: true, completion: nil)
-            
-        case 3:
-            let storyObject = UIStoryboard(name: "Zahid", bundle: nil)
-            let webKitVC = storyObject.instantiateViewController(withIdentifier: "webScreen") as! WebUIKit
-         
-            webKitVC.bookLink = genBookLink![indexPath.row]
-            
-            self.present(webKitVC, animated: true, completion: nil)
-            
-        case 4:
-            let storyObject = UIStoryboard(name: "Zahid", bundle: nil)
             let pdfScreenVC = storyObject.instantiateViewController(withIdentifier: "pdfScreen") as! PdfScreenViewController
-            pdfScreenVC.pdfName = localBooks[0]
+            pdfScreenVC.pdfName = bookData[indexPath.row]
             self.present(pdfScreenVC, animated: true, completion: nil)
             
-            
-            
-        default:
-            print("No Book")
-        }
-    }
-        
-        
-        if collectionView == self.CV2{
-        switch indexPath.row{
-        case 0 :
-            
-            let storyObject = UIStoryboard(name: "Zahid", bundle: nil)
-            let webKitVC = storyObject.instantiateViewController(withIdentifier: "webScreen") as! WebUIKit
-         
-            webKitVC.bookLink = techBookLink![indexPath.row]
-            
-            self.present(webKitVC, animated: true, completion: nil)
-            
-            
         case 1:
-            let storyObject = UIStoryboard(name: "Zahid", bundle: nil)
-            let webKitVC = storyObject.instantiateViewController(withIdentifier: "webScreen") as! WebUIKit
-          
-            webKitVC.bookLink = techBookLink![indexPath.row]
             
-            self.present(webKitVC, animated: true, completion: nil)
-            
-
-        case 2:
-            let storyObject = UIStoryboard(name: "Zahid", bundle: nil)
-            let webKitVC = storyObject.instantiateViewController(withIdentifier: "webScreen") as! WebUIKit
-            
-            webKitVC.bookLink = techBookLink![indexPath.row]
-            
-            self.present(webKitVC, animated: true, completion: nil)
-            
-            
-        case 3:
-            let storyObject = UIStoryboard(name: "Zahid", bundle: nil)
-            let webKitVC = storyObject.instantiateViewController(withIdentifier: "webScreen") as! WebUIKit
-         
-            webKitVC.bookLink = genBookLink![indexPath.row]
-            
-            self.present(webKitVC, animated: true, completion: nil)
-            
-        case 4:
             let storyObject = UIStoryboard(name: "Zahid", bundle: nil)
             let pdfScreenVC = storyObject.instantiateViewController(withIdentifier: "pdfScreen") as! PdfScreenViewController
-            pdfScreenVC.pdfName = localBooks[1]
+            pdfScreenVC.pdfName = bookData[indexPath.row]
             self.present(pdfScreenVC, animated: true, completion: nil)
-            
-            
-            
-        default:
-            print("No Book")
-        }
-    }
-        
-        
-        if collectionView == self.CV3{
-        switch indexPath.row{
-        case 0 :
-            
-            let storyObject = UIStoryboard(name: "Zahid", bundle: nil)
-            let webKitVC = storyObject.instantiateViewController(withIdentifier: "webScreen") as! WebUIKit
-          
-            webKitVC.bookLink = recBookLink![indexPath.row]
-            
-            self.present(webKitVC, animated: true, completion: nil)
-            
-            
-        case 1:
-            let storyObject = UIStoryboard(name: "Zahid", bundle: nil)
-            let webKitVC = storyObject.instantiateViewController(withIdentifier: "webScreen") as! WebUIKit
-   
-            webKitVC.bookLink = recBookLink![indexPath.row]
-            
-            self.present(webKitVC, animated: true, completion: nil)
-            
 
         case 2:
-            let storyObject = UIStoryboard(name: "Zahid", bundle: nil)
-            let webKitVC = storyObject.instantiateViewController(withIdentifier: "webScreen") as! WebUIKit
-         
-            webKitVC.bookLink = recBookLink![indexPath.row]
             
-            self.present(webKitVC, animated: true, completion: nil)
-            
-            
-        case 3:
-            let storyObject = UIStoryboard(name: "Zahid", bundle: nil)
-            let webKitVC = storyObject.instantiateViewController(withIdentifier: "webScreen") as! WebUIKit
-         
-            webKitVC.bookLink = genBookLink![indexPath.row]
-            
-            self.present(webKitVC, animated: true, completion: nil)
-            
-        case 4:
             let storyObject = UIStoryboard(name: "Zahid", bundle: nil)
             let pdfScreenVC = storyObject.instantiateViewController(withIdentifier: "pdfScreen") as! PdfScreenViewController
-            pdfScreenVC.pdfName = localBooks[2]
+            pdfScreenVC.pdfName = bookData[indexPath.row]
             self.present(pdfScreenVC, animated: true, completion: nil)
             
             
@@ -242,15 +81,13 @@ class BooksViewController: UIViewController, UICollectionViewDelegate, UICollect
             print("No Book")
         }
     }
-}
         
+        
+
     
 
           override func viewDidLoad() {
-              super.viewDidLoad()
-              
-              viewDidLoad_TransparentNavBar()
-             // viewDidLoad_PopulateCurrentUserName()
+        super.viewDidLoad()
               
               title = "Books"
 
@@ -262,111 +99,18 @@ class BooksViewController: UIViewController, UICollectionViewDelegate, UICollect
               
               CV3.delegate = self
               CV3.dataSource = self
-              
-              var fetchBooks = FetchBooks()
-//              var imgLinks = ImageLinks(smallThumbnail: "", thumbnail: "")
-//              var volInfo = VolumeInfo(title: "", description: "")
-//              var books = Books(id: "", selfLink: "")
-//              var bModel = BookModel(items: [books])
-              
-              fetchBooks.getGeneral(completion: {
-                  result in
-                  switch result{
-                  case .failure(let error):
-                      print("Error Message",error)
-                  case .success(let res):
-                      for item in res.items {
-                          if genBookLink == nil{
-                              genBookLink = [item.volumeInfo.previewLink]
-                          }else{
-                          genBookLink?.append(item.volumeInfo.previewLink)
-                              
-                          }
-                      }
-                }
-            })
-              fetchBooks.getTechnical(completion: {
-                  result in
-                  switch result{
-                  case .failure(let error):
-                      print("Error Message",error)
-                  case .success(let res):
-                      for item in res.items {
-                          if techBookLink == nil{
-                              techBookLink = [item.volumeInfo.previewLink]
-                          }else{
-                              techBookLink?.append(item.volumeInfo.previewLink)
-                              
-                          }
-                      }
-                }
-            })
-              fetchBooks.getRecipes(completion: {
-                  result in
-                  switch result{
-                  case .failure(let error):
-                      print("Error Message",error)
-                  case .success(let res):
-                      for item in res.items {
-                          if recBookLink == nil{
-                              recBookLink = [item.volumeInfo.previewLink]
-                          }else{
-                          recBookLink?.append(item.volumeInfo.previewLink)
-                              
-                          }
-                      }
-                }
-            })
-              
-             // var timer = Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(updateBooks), userInfo: nil, repeats: true)
-              
-//             let generalBooks = FetchBooks.fetchBooks.getGeneral()
-//             let technicalBooks = FetchBooks.fetchBooks.getTechnical()
-//             let recipesBooks = FetchBooks.fetchBooks.getRecipes()
     }
     
-    @objc func updateBooks(){
-//        if (bookLink != nil){
-//
-//            let newUrl = URL(string: "http://books.google.ca/books?id=xrD1iuM_2LgC&printsec=frontcover&hl=&source=gbs_api")
-//            webKitView.load(URLRequest(url: newUrl!))
-//            self.view = webKitView
-            
-        }
+
     
-    //Gives transparent nav bar
-    func viewDidLoad_TransparentNavBar() {
-            let appearance = UINavigationBarAppearance()
-            appearance.configureWithTransparentBackground()
-            UINavigationBar.appearance().standardAppearance = appearance
-        }
-    
-    
-//    func viewDidLoad_PopulateCurrentUserName() {
-//            // TODO: - Uncomment the following two lines to populate user name in the nav bar
-//            CurrentUser.user.updateCurrentUserName()
-//            navItemUserName.title = CurrentUser.user.name
-    
-    
-    
+
 }
 
-//extension BooksViewController : UISearchBarDelegate{
-//    func searchBar(_ searchBar: UISearchBar, textDidChange searchText : String){
-//        bookData.removeAll()
-//        print("in search")
-//        if searchText == "" {
-//            searching = false
-//            FetchBooks.fetchBooks =
-//        }
+//extension BooksViewController : UICollectionViewDelegateFlowLayout{
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        return CGSize()
 //    }
 //}
-    
-
-    
-
-
-
-
 
 
